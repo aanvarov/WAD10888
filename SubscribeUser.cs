3e66ee383f05598e4e930557600301b2c661aaac
Abror Anvarov <46844042+aanvarov@users.noreply.github.com>
2021-03-12 22:43:07 +0500

    Add files via upload

---
+++ SubscribeUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WAD._10888.APP
+{
+    public class SubscribeUser
+    {
+        public static void SubscribeLibUser(string emailAddress)
+        {
+            Console.WriteLine(emailAddress);
+        }
+    }
+}
