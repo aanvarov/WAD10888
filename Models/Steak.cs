395c8e60e39bdd4018ec4eb286bfe62694087586
Abror Anvarov <46844042+aanvarov@users.noreply.github.com>
2021-04-20 14:08:23 +0500

    Add files via upload

---
+++ Models/Steak.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD.WebApp._10888.Models
+{
+    public class Steak
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(25)]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public SteakSize Size { get; set; }
+
+    }
+
+    public enum SteakSize
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
---
+++ Models/SteakDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD.WebApp._10888.Models
+{
+    public class SteakDbContext : DbContext
+    {
+        public SteakDbContext()
+        {
+
+        }
+
+        public DbSet<Steak> Steak { get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
